ecf4f1628c60e1ee311e03d1998448cd571ef5e0
Dmannen03 <39675750+Dmannen03@users.noreply.github.com>
2021-02-15 16:56:41 +0100

    implemented zoom

--- main.lua
+++ main.lua
@@ -5,25 +5,48 @@ love.load = function()
     love.window.setMode(love.graphics.getWidth(),love.graphics.getWidth()*19/16,{fullscreen = true});
 
     gameWidth = 1920;
-    worldScale = 3;
-    screenScale = love.graphics.getWidth()/gameWidth;
-    mouseScale = 1/screenScale/worldScale;
+
+    screenScale = 1/(love.graphics.getWidth()/gameWidth);
+    zoom = 1;
+
+    translate = {x = 0, y = 0};
+ 
+
     rectangles = {};
-    
+  
 end    
 
-function love.mousepressed(x,y,button)
+function screenToWorldcords(x,y)
+
+    return {x = x/screenScale/zoom - translate.x, y = y/screenScale/zoom - translate.y};
+    
+end
+
+function worldToScreencords(x,y)
+
+    return x*screenScale*zoom, y*screenScale*zoom;
+    
+end
+
+
+
+function love.mousepressed(mousex,mousey,button)
   
+
+    local x =  screenToWorldcords(mousex,mousey).x;
+    local y =  screenToWorldcords(mousex,mousey).y;
+
     for i = #rectangles, 1, -1 do
 
-        if(button == 1 and x*mouseScale > rectangles[i].x and x*mouseScale < (rectangles[i].x + rectangles[i].width) and y*mouseScale > rectangles[i].y and y*mouseScale < (rectangles[i].y + rectangles[i].height)) then
+
+        if(button == 1 and x > rectangles[i].x and x < (rectangles[i].x + rectangles[i].width) and y > rectangles[i].y and y < (rectangles[i].y + rectangles[i].height)) then
             if (rectangles[i].clicked == false) then    
                 rectangles[i].clicked = true;
 
             end
     
-            rectangles[i].clickedOffsetX = x*mouseScale - rectangles[i].x;
-            rectangles[i].clickedOffsetY = y*mouseScale - rectangles[i].y;
+            rectangles[i].clickedOffsetX = x - rectangles[i].x;
+            rectangles[i].clickedOffsetY = y - rectangles[i].y;
 
             rectangles = setfirst(rectangles,i);
             break
@@ -47,23 +70,35 @@ function love.keypressed(key)
 
     if(key == "escape")then love.event.quit(); end
 
+    if(key == "q")then translate.x = translate.x + 10; end
+
 end
 
 function love.wheelmoved(x,y)
 
-    worldScale = worldScale + y/10;
-    mouseScale = 1/screenScale/worldScale;
 
-    print(worldScale);
-    
+    local bmouse = screenToWorldcords(love.mouse.getX(),love.mouse.getY());
+
+    zoom = zoom + 0.1*y;
+ 
+    local amouse = screenToWorldcords(love.mouse.getX(),love.mouse.getY());
+
+    local d = {x = amouse.x - bmouse.x, y = amouse.y - bmouse.y}; 
+
+    translate.x = translate.x + d.x;
+    translate.y = translate.y + d.y;
+
 end
 
 love.update = function(dt)
 
     for i = 1, #rectangles do
         if(rectangles[i].clicked) then
-            rectangles[i].x = love.mouse.getX()*mouseScale - rectangles[i].clickedOffsetX;
-            rectangles[i].y = love.mouse.getY()*mouseScale - rectangles[i].clickedOffsetY;
+            local x = screenToWorldcords(love.mouse.getX(),love.mouse.getY()).x;
+            local y = screenToWorldcords(love.mouse.getX(),love.mouse.getY()).y;
+
+            rectangles[i].x = x - rectangles[i].clickedOffsetX;
+            rectangles[i].y = y - rectangles[i].clickedOffsetY;
         end 
     end
 end
@@ -71,12 +106,11 @@ end
 love.draw = function()
 
     love.graphics.scale(screenScale,screenScale);
+
     love.graphics.push();
 
-    
-    
-    
-    love.graphics.scale(worldScale,worldScale);
+    love.graphics.scale(zoom,zoom);
+    love.graphics.translate(translate.x,translate.y);
 
     for i = 1, #rectangles do
     
@@ -101,7 +135,7 @@ end
 function createrectangle()
 
 
-    rectangles[#rectangles + 1] = {x = love.mouse.getX()/mouseScale, y = love.mouse.getY()/mouseScale, width = 200, height = 200, clicked = true,clickedOffsetY = 100, clickedOffsetX = 100}; 
+    rectangles[#rectangles + 1] = {x = love.mouse.getX(), y = love.mouse.getY(), width = 100, height = 100, clicked = true,clickedOffsetY = 50, clickedOffsetX = 50}; 
     
 end
 
